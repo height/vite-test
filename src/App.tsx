a78a537cc25a7c0402fcbc906762255489d7050d
Link <pk.link@163.com>
2024-06-16 10:30:58 +0800

    feat: update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import { useState } from 'react'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+import { TabBar } from "antd-mobile";
+import { AddSquareOutline, SetOutline } from "antd-mobile-icons";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.styl";
+
+const tabs = [
+  {
+    key: "/",
+    title: "头衔申请",
+    icon: <AddSquareOutline />,
+  },
+  {
+    key: "/settings",
+    title: "国王管理",
+    icon: <SetOutline />,
+  },
+];
+
+function AppContent() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { pathname } = location;
+
+  const setRouteActive = (value: string) => {
+    navigate(value);
+  };
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+      <div className="tab-bar">
+        <TabBar activeKey={pathname} onChange={(key) => setRouteActive(key)}>
+          {tabs.map((tab) => (
+            <TabBar.Item key={tab.key} icon={tab.icon} title={tab.title} />
+          ))}
+        </TabBar>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+export default function Home() {
+    return <div>
+        home!!!
+    </div>
+}
